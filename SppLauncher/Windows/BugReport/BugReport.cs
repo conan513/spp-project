b9a61705688ffecec0bc646da63d727880dddff2
whit33r <whit33r@gmail.com>
2013-08-18 19:41:50 +0200

    improve bugreport send method

--- SppLauncher/Windows/BugReport/BugReport.cs
+++ SppLauncher/Windows/BugReport/BugReport.cs
@@ -2,8 +2,6 @@
 using System.ComponentModel;
 using System.IO;
 using System.Net;
-using System.Net.Sockets;
-using System.Text;
 using System.Windows.Forms;
 
 namespace SppLauncher.Windows.BugReport
@@ -15,56 +13,16 @@ namespace SppLauncher.Windows.BugReport
         public static string lol;
         private GetSysInfo getSys;
         private SendReport send;
+        private GetServerIP ip;
         public BugReport()
         {
             InitializeComponent();
             getSys = new GetSysInfo();
             send = new SendReport();
+            ip = new GetServerIP();
             cbBugType.Text = "Aibot";
             
             bwGetSysInfo.RunWorkerAsync();
-        }
-
-        public Cursor SetCursor
-        {
-            set { Cursor = value; }
-            get { throw new NotImplementedException(); }
-        }
-
-
-        public static string test
-        {
-            get { return null; }
-            set { ; }
-        }
-
-        public void SendError(string report,string bugtype, string email, string desc, string cpu, string cpucore, string ram,string os, string ver)
-        {
-            try
-            {
-                
-                TcpClient client = new TcpClient();
-                IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(RemoteIp), 5459);
-                client.Connect(serverEndPoint);
-                NetworkStream clientStream = client.GetStream();
-
-                UTF8Encoding encoder = new UTF8Encoding();
-                byte[] buffer        = encoder.GetBytes(report + ";" + bugtype + ";" + email + ";" + desc + ";" + cpu + ";" + cpucore + ";" + ram + ";" + os + ";" + ver);
-                
-                clientStream.Write(buffer, 0, buffer.Length);
-                clientStream.Flush();
-                client.Close();
-                this.Cursor = Cursors.Default;
-                MessageBox.Show("Thank you for the report.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                this.Close();
-            }
-            catch (Exception)
-            {
-                this.Cursor = Cursors.Default;
-                MessageBox.Show("Can not connect to the reporting server\nPlease try again later", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-
-            this.Cursor = Cursors.Default;
         }
 
         private void btnSend_Click(object sender, EventArgs e)
@@ -82,18 +40,7 @@ namespace SppLauncher.Windows.BugReport
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
-            try
-            {
-                var client    = new WebClient();
-                Stream stream = client.OpenRead("http://dl.dropbox.com/u/7587303/Updates/ip.txt");
-                var reader    = new StreamReader(stream);
-                RemoteIp      = reader.ReadToEnd();
-                
-            }
-            catch (Exception)
-            {
-            }
-
+            RemoteIp = ip.Getip();
             textBox1.Text = "Loading...";
             textBox1.Text = "Cpu: " + getSys.GetProcessorNameL() + Environment.NewLine + "Core: " + count + Environment.NewLine + "Total Memory: " + getSys.getmemory() + "Mb" + Environment.NewLine + "Operation System: " + getSys.getOS();
         }
---
+++ SppLauncher/Windows/BugReport/GetServerIP.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace SppLauncher.Windows.BugReport
+{
+    class GetServerIP
+    {
+
+        public string Getip()
+        {
+            try
+            {
+                var client = new WebClient();
+                Stream stream = client.OpenRead("http://dl.dropbox.com/u/7587303/Updates/ip.txt");
+                var reader = new StreamReader(stream);
+                string ip = reader.ReadToEnd();
+                return ip != "" ? ip : null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+    }
+}
